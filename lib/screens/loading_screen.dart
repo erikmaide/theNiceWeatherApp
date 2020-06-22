32edac76f267b765246d93466c0dccb6956d5553
Erik Maide <maide.erik@gmail.com>
2020-06-23 01:15:48 +0300

    add expection handler for getLocation()

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -7,28 +7,29 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
-    Geolocator geolocator = Geolocator();
-    GeolocationStatus geolocatorPerm =
-        await geolocator.checkGeolocationPermissionStatus();
-    print(geolocatorPerm);
-    Position position = await geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    print('This is my location:');
-    print(position);
+    try {
+      Geolocator geolocator = Geolocator();
+      GeolocationStatus geolocatorPerm =
+          await geolocator.checkGeolocationPermissionStatus();
+      print(geolocatorPerm);
+      Position position = await geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      print('This is my location:');
+      print(position);
+    } catch (error) {
+      print(error);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
