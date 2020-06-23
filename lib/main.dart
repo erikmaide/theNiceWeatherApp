c933e05f390f30fd4b13cbd35cacb04f1eeb326b
Erik Maide <maide.erik@gmail.com>
2020-06-23 12:14:31 +0300

    add enviroment variable as api key, additional tweaks

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
+import 'package:flutter_config/flutter_config.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_config/flutter_config.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,6 +14,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
+  }
+
+  void getData() async {
+    String apiKey = FlutterConfig.get('API_KEY');
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=$apiKey');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+      int condition = decodedData['weather'][0]['id'];
+      double temperature = decodedData['main']['temp'];
+      String city = decodedData['name'];
+      print('The key values are: $city, $temperature, $condition');
+    } else {
+      print(response.statusCode);
+    }
   }
 
   void getLocation() async {
